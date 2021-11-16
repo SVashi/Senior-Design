251c503323c591644a289a8ab67cf1ed2d7b7a78
fnbates <fnbates@iastate.edu>
2021-11-16 13:22:53 -0600

    Added GPIO_INIT source and header files

---
+++ Project/GPIO_INIT.cpp
@@ -0,0 +1,12 @@
+
+#include "GPIO_INIT.h"
+/*
+ * GPIO_INIT.cpp
+ *
+ *  Created on: Nov 16, 2021
+ *      Author: fnbat
+ */
+
+
+
+
---
+++ Project/GPIO_INIT.h
@@ -0,0 +1,18 @@
+#include <msp430.h>
+#include "stdint.h"
+
+/*
+ * GPIO_INIT.h
+ *
+ *  Created on: Nov 16, 2021
+ *      Author: fnbat
+ */
+
+#ifndef GPIO_INIT_H_
+#define GPIO_INIT_H_
+
+
+
+
+
+#endif /* GPIO_INIT_H_ */
