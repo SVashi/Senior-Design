9728668d91ccc28456fc21e441e23c7d0022a101
fnbates <fnbates@iastate.edu>
2021-12-07 13:33:41 -0600

    Changed states/imgs to decrease screen refreshes

--- Project/RoomChallenge.cpp
+++ Project/RoomChallenge.cpp
@@ -15,20 +15,18 @@ void RoomChallenge()
     uint8_t minButtonVal =0;
     clearButtonPress();
     //challenge determination
-    EPD_FullScreen(IMAGE_ROOM1);
     if(getRoomChallenge()>0){           //challenge 1
-        EPD_FullScreen(IMAGE_C2);
+        EPD_FullScreen(IMAGE_R2);
         minButtonVal = 5;
         clearRoomChallenge();
     } else{                             //challenge 2
-        EPD_FullScreen(IMAGE_C1);
+        EPD_FullScreen(IMAGE_R1);
         minButtonVal = 10;
         addRoomChallenge();
     }
     while(getData() < minButtonVal){    //enter LPM until challenge completed
         __low_power_mode_3();
     }
-    EPD_FullScreen(IMAGE_RMSUC);
     addSucceed();
 }
 
