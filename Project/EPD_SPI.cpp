9e0a92c14d910cc03397f3c8dad9973901bbd55f
market626 <48298634+market626@users.noreply.github.com>
2021-12-09 13:11:36 -0600

    stuf

--- Project/EPD_SPI.cpp
+++ Project/EPD_SPI.cpp
@@ -302,12 +302,7 @@ void EPD_Sleep(void){
 void EPD_FullScreen(const uint8_t *image){
     uint8_t old = getOldState();
     uint8_t current = getState();
-    //check if the current screen does not match current
-<<<<<<< HEAD
-    uint8_t old = getOldState();
-    uint8_t current = getState();
-=======
->>>>>>> branch 'main' of https://git.ece.iastate.edu/svashi/ccs-senior-design.git
+
     if(old != current){
     EPD_Init();
     EPD_DisplayImage(image);
