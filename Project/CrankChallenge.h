a59d96c591b3ca0fead485ab2009a6e671d8a6cd
market626 <48298634+market626@users.noreply.github.com>
2021-12-09 12:02:33 -0600

    Turned low power mode (case 5) into function called CrankChallenge

--- Project/CrankChallenge.h
+++ Project/CrankChallenge.h
@@ -1,13 +1,13 @@
-/*
- * CrankChallenge.h
- *
- *  Created on: Dec 9, 2021
- *      Author: mlmj1
- */
+#include <ctpl.h>
+#include <msp430.h>
+#include "EPD_SPI.h"
+#include "IMG.h"
+#include "Game_State.h"
 
 #ifndef CRANKCHALLENGE_H_
 #define CRANKCHALLENGE_H_
 
+void CrankChallenge(uint16_t millis);
 
 
 
